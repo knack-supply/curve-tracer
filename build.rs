5f55484e0e8c27f093ac3d6a6f6ebe10ba59fc18
Ilya Epifanov <elijah.epifanov@gmail.com>
2019-02-25 23:37:16 +0100

    removed cargo git-version

--- build.rs
+++ build.rs
@@ -1,11 +1,7 @@
 extern crate winres;
-extern crate git_version;
-
 use failure::Error;
 
 fn main() -> Result<(), Error> {
-    git_version::set_env();
-
     if cfg!(target_os = "windows") {
         let mut res = winres::WindowsResource::new();
         res.set_icon("res/icon256.ico");
